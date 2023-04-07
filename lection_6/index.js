29d4bf72bb865ab68a661089f04fdb9297469592
Oleh Zankiv <oleh.zankiv@zealicsolutions.com>
2023-04-07 21:35:40 +0300

    Lection 6 finish

--- lection_6/index.js
+++ lection_6/index.js
@@ -1,5 +1,98 @@
 //
 // QUESTIONS ==>
+
+// rest/... - збирання багато значень в одне занення
+// spread/... - розбирає з одного значення в багато змінних
+
+// const userFromDataBase = {
+//   name: "Oleh",
+//   age: 22,
+//   position: "teacher",
+// };
+
+// const car = {};
+
+// const { aaa: bbb } = { aaa: "BBB" };
+
+// console.log(bbb);
+
+// const {
+//   car: newCar,
+//   name,
+//   age = 24,
+// } = {
+//   name: "Ivan",
+//   car: {
+//     model: "BMW",
+//   },
+// };
+
+// console.log(age);
+// const { age, name, position } = userFromDataBase;
+
+// const adminUser = {
+//   isAdmin: true,
+//   ...userFromDataBase,
+// };
+
+// function showUserInfo({ age, name, position, surname }) {
+//   console.log(
+//     `My name is ${name}. My surname is ${surname}. I'm ${age}. I'm ${position}.`
+//   );
+// }
+
+// showUserInfo({
+//   age: 22,
+//   name: "Oleh",
+//   surname: "Zankiv",
+//   position: "Teacher",
+// });
+
+// adminUser.name = userFromDataBase.name
+// adminUser.age = userFromDataBase.age
+
+// function sum(a, b, ...rest) {
+//   console.log(a, b, c, rest);
+//   return a + b;
+// }
+
+// // sum(1, 2, 2, 23, 2, 323, 23, 2, 32);
+
+// const numbers = [4, 3, 6, 5];
+
+// sum(1, 2, ...numbers);
+
+// const a = [1, 2, 3]; // apwodkwpodkpwodkwopk
+// const b = [...a]; // 8h3uhdnbuhsfefuheuhf
+
+// console.log(a === b);
+
+// const obj = {
+//   name: "Ivan",
+//   name: "Anna",
+// };
+
+// console.log(obj);
+
+// const f = { A: 1, B: 2 };
+// const s = { A: 3, C: 4 };
+
+// const res = {
+//   A: 4,
+//   // { A: 4 }
+
+//   ...f, // { A: 1, B: 2 };
+//   // { A: 1, B: 2 }
+
+//   ...s, // { A: 3, C: 4 };
+//   //  { B: 2, A: 3, C: 4 }
+
+//   C: 6,
+//   //  { B: 2, A: 3, C: 6 }
+// };
+
+// console.log(res);
+
 // <=== QUESTIONS
 
 // Модуль 3 Заняття 6. Деструктуризація та rest/spread
@@ -7,81 +100,83 @@
 // # ->
 // Example 1 - Деструктуризація
 // Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору незалежних аргументів.
-// function calcBMI(weight, height) {
-//   const numericWeight = Number(weight.replace(',', '.'));
-//   const numericHeight = Number(height.replace(',', '.'));
-//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
-// }
+function calcBMI({ weight, height }) {
+  const numericWeight = Number(weight.replace(",", "."));
+  const numericHeight = Number(height.replace(",", "."));
 
-// // Було
-// // console.log(calcBMI('88,3', '1.75'));
-// // console.log(calcBMI('68,3', '1.65'));
-// // console.log(calcBMI('118,3', '1.95'));
+  return Number((numericWeight / numericHeight ** 2).toFixed(1));
+}
 
-// // Очікується
+// Було
+// console.log(calcBMI("88,3", "1.75"));
+// console.log(calcBMI("68,3", "1.65"));
+// console.log(calcBMI("118,3", "1.95"));
+
+// Очікується
 // console.log(
 //   calcBMI({
-//     weight: '88,3',
-//     height: '1.75',
-//   }),
+//     weight: "88,3",
+//     height: "1.75",
+//   })
 // );
 // console.log(
 //   calcBMI({
-//     weight: '68,3',
-//     height: '1.65',
-//   }),
+//     weight: "68,3",
+//     height: "1.65",
+//   })
 // );
 // console.log(
 //   calcBMI({
-//     weight: '118,3',
-//     height: '1.95',
-//   }),
+//     weight: "118,3",
+//     height: "1.95",
+//   })
 // );
 
 // # ->
 // Example 2 - Деструктуризація
 // Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору незалежних аргументів.
 
-// function printContactsInfo(names, phones) {
-//   const nameList = names.split(',');
-//   const phoneList = phones.split(',');
+// function printContactsInfo({ names, phones }) {
+//   const nameList = names.split(",");
+//   const phoneList = phones.split(",");
+
 //   for (let i = 0; i < nameList.length; i += 1) {
 //     console.log(`${nameList[i]}: ${phoneList[i]}`);
 //   }
 // }
 
 // // Було
-// // printContactsInfo(
-// //   'Jacob,William,Solomon,Artemis',
-// //   '89001234567,89001112233,890055566377,890055566300',
-// // );
+// printContactsInfo(
+//   "Jacob,William,Solomon,Artemis",
+//   "89001234567,89001112233,890055566377,890055566300"
+// );
 
-// // Очікується
+// Очікується
 // printContactsInfo({
-//   names: 'Jacob,William,Solomon,Artemis',
-//   phones: '89001234567,89001112233,890055566377,890055566300',
+//   names: "Jacob,William,Solomon,Artemis",
+//   phones: "89001234567,89001112233,890055566377,890055566300",
 // });
 
 // # ->
 // Example 3 - Глибока деструктуризація
 // Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору незалежних аргументів.
 
-// function getBotReport(companyName, repairBots, defenceBots) {
-//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// function getBotReport({ companyName, bots: { repair, defence } }) {
+//   return `${companyName} has ${repair + defence} bots in stock`;
 // }
 
-// // Було
-// // console.log(getBotReport('Cyberdyne Systems', 150, 50));
+// Було
+// console.log(getBotReport("Cyberdyne Systems", 150, 50));
 
-// // Очікується
+// Очікується
 // console.log(
 //   getBotReport({
-//     companyName: 'Cyberdyne Systems',
+//     companyName: "Cyberdyne Systems",
 //     bots: {
 //       repair: 150,
 //       defence: 50,
 //     },
-//   }),
+//   })
 // ); // "Cyberdyne Systems has 200 bots in stock"
 
 // # ->
@@ -90,34 +185,42 @@
 // щоб вона приймала об'єкт параметрів із властивостями companyName та stock
 // та виводила репорт про кількість товарів на складі будь-якої компанії.
 
-// // Рішення
-// function getStockReport({ companyName, stock }) {
+// function getStockReport({ companyName, stock, productName = "products" }) {
 //   let total = 0;
+
 //   for (const value of Object.values(stock)) {
 //     total += value;
 //   }
-//   return `${companyName} has ${total} items in stock`;
+
+//   //   for (const key in stock) {
+//   //     if (stock.hasOwnProperty(key)) {
+//   //       total += stock[key];
+//   //     }
+//   //   }
+
+//   return `${companyName} has ${total} ${productName} in stock`;
 // }
 
 // console.log(
 //   getStockReport({
-//     companyName: 'Cyberdyne Systems',
+//     companyName: "Cyberdyne Systems",
+//     productName: "bots",
 //     stock: {
 //       repairBots: 150,
 //       defenceBots: 50,
 //     },
-//   }),
+//   })
 // ); // "Cyberdyne Systems has 200 items in stock"
 
 // console.log(
 //   getStockReport({
-//     companyName: 'Belacci',
+//     companyName: "Belacci",
 //     stock: {
 //       shoes: 20,
 //       skirts: 10,
 //       hats: 5,
 //     },
-//   }),
+//   })
 // ); // "Belacci has 35 item in stock"
 
 // # ->
@@ -126,63 +229,109 @@
 // щоб вона повертала новий об'єкт контакту з доданими властивостями id та createdAt,
 // а також list зі значенням "default" якщо в partialContact немає такої властивості.
 
-// // Рішення
 // function createContact(partialContact) {
 //   return {
-//     list: 'default',
-//     ...partialContact,
 //     id: generateId(),
-//     createdAt: Date.now(),
+//     createdAt: Date.now(), // timestamp [number] from 1970 year
+//     list: "default",
+//     ...partialContact,
 //   };
 // }
 
+// const contact = {
+//   name: "Mango",
+//   email: "mango@mail.com",
+//   list: "friends",
+// };
+
+// console.log(createContact(contact));
 // console.log(
 //   createContact({
-//     name: 'Mango',
-//     email: 'mango@mail.com',
-//     list: 'friends',
-//   }),
-// );
-// console.log(
-//   createContact({
-//     name: 'Poly',
-//     email: 'poly@hotmail.com',
-//   }),
+//     name: "Poly",
+//     email: "poly@hotmail.com",
+//   })
 // );
 
 // function generateId() {
-//   return '_' + Math.random().toString(36).substr(2, 9);
+//   return "_" + Math.random().toString(36).substr(2, 9);
+//   // Як перевести з десяткової системи в двійкову?
+//   // Як перевести з десяткової системи в 36-у?
 // }
 
 // # ->
 // Example 6 - Операція rest
 // Напиши функцію transformUsername(user) так,
-// щоб вона повертала новий об'єкт із властивістю fullName, замість firstName та lastName.
+// щоб вона повертала новий об'єкт із властивістю fullName,
+// замість firstName та lastName.
 
-// // Рішення
-// function transformUsername({ firstName, lastName, ...otherProps }) {
+// function transformUsername({
+//   firstName = "Default",
+//   lastName = "",
+//   ...newUser
+// }) {
 //   return {
-//     fullName: `${firstName} ${lastName}`,
-//     ...otherProps,
+//     fullName: firstName + " " + lastName,
+//     createdAt: Date.now(),
+//     ...newUser,
+//     updatedAt: Date.now(),
 //   };
 // }
 
-// console.log(
-//   transformId({
-//     id: 1,
-//     firstName: 'Jacob',
-//     lastName: 'Mercer',
-//     email: 'j.mercer@mail.com',
-//     friendCount: 40,
-//   }),
-// );
+// const user = transformUsername({
+//   id: 1,
+//   firstName: "Jacob",
+//   lastName: "Mercer",
+//   email: "j.mercer@mail.com",
+//   friendCount: 40,
+// });
+
+// console.log(user);
+
+// console.log(transformUsername(user));
 
 // console.log(
-//   transformId({
+//   transformUsername({
 //     id: 2,
-//     firstName: 'Adrian',
-//     lastName: 'Cross',
-//     email: 'a.cross@hotmail.com',
+//     firstName: "Adrian",
+//     lastName: "Cross",
+//     email: "a.cross@hotmail.com",
 //     friendCount: 20,
-//   }),
+//   })
 // );
+
+// "не можу зрозуміти що не так.
+// Напиши функцію getAllPropValues(propName),
+// яка приймає один параметр propName - ім'я (ключ) властивості.
+// Функція повинна повернути масив всіх значень властивості з
+// таким ім'ям з кожного об'єкта в масиві products.
+// Якщо в об'єктах відсутні властивості з таким ім'ям,
+// функція повинна повернути порожній масив.
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   const propValues = [];
+
+//   for (const product of products) {
+//     const value = product[propName];
+//     if (value) propValues.push(value);
+//   }
+
+//   return propValues;
+// }
+
+// // Виклик getAllPropValues("name") повертає ["Radar", "Scanner", "Droid", "Grip"]
+// // Виклик getAllPropValues("quantity") повертає [4, 3, 7, 9]
+// // Виклик getAllPropValues("price") повертає [1300, 2700, 400, 1200]
+// // Виклик getAllPropValues("category") повертає []
+
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("price"));
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("category"));
+// // не приймає автоперевірка"
